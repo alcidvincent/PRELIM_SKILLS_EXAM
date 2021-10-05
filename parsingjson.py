9c3e1ea93e519b08eb36504e7aa5534d2e8a90d5
alcidvincent <alcidvincent@gmail.com>
2021-10-05 02:37:06 +0000

    Alcid, Eugene Vincent C. Files Added

---
+++ parsingjson.py
@@ -0,0 +1,23 @@
+import json
+import csv
+
+with open ('covid_cases.json','r') as json_file:
+    ourjson = json.load(json_file)
+
+covid_data = ourjson['records']
+
+data_file = open('data_file.csv', 'w')
+
+csv_writer = csv.writer(data_file)
+
+count = 0
+
+for i in covid_data:
+    if count == 0:
+        header = i.keys()
+        csv_writer.writerow(header)
+        count += 1
+
+    csv_writer.writerow(i.values())
+
+data_file.close()
